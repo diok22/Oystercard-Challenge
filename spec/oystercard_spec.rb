e9230746fd40a2db031c9fb00e00ebdc6c95eb0b
Dionysis <Dionysis.k22@gmail.com>
2016-10-03 15:51:35 +0100

    added topup method

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -8,4 +8,10 @@ describe Oystercard do
 		expect(oystercard.balance).to eq(0)
 	end
 
+	it 'tops up oystercard with given amount' do
+		amount = rand(5..20) # can take various amounts
+		oystercard.top_up(amount)
+		expect(oystercard.balance).to eq(amount)
+	end
+
 end
