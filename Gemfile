34a33a5ee126bc51621517bd4a66a2f412399bcf
Dionysis <Dionysis.k22@gmail.com>
2016-10-06 12:30:41 +0100

    deleted journey_log lib and spec files due to rspec segmentation fault error

--- Gemfile
+++ Gemfile
@@ -6,7 +6,6 @@ group :test do
   gem 'rspec'
 end
 
-group :test, :development do
+group :development do
   gem 'rspec'
-  gem 'coveralls', require: false
 end
--- spec/journey_spec.rb
+++ spec/journey_spec.rb
@@ -4,50 +4,50 @@ require 'station'
 
 describe Journey do
 
-  subject(:journey_history) {described_class.new}
+  subject(:journey) {described_class.new}
 
-  # before :each do
-  #   @station1 = Station.new('Paddington',1)
-  #   @station2 = Station.new('Waterloo',2)
-  # end
+  before :each do
+    @station1 = Station.new('Paddington',1)
+    @station2 = Station.new('Waterloo',2)
+  end
 
   describe "#fare" do
-    # it "deducts the minimum fare for a single trip" do
-    #   journey_history.start_journey(@station1)
-    #   journey_history.end_journey(@station2)
-    #   expect(journey_history.fare).to eq Journey::MINIMUM_FARE
-    # end
+    it "deducts the minimum fare for a single trip" do
+      journey.start_journey(@station1)
+      journey.end_journey(@station2)
+      expect(journey.fare).to eq Journey::MINIMUM_FARE
+    end
 
     it 'deducts the penalty fare for incomplete journey' do
-      journey_history.start_journey(@station1)
-      expect(journey_history.fare).to eq Journey::DEFAULT_PENALTY
+      journey.start_journey(@station1)
+      expect(journey.fare).to eq Journey::DEFAULT_PENALTY
     end
 
   end
 
   describe '#clear_current_journey' do
     it "last journey hash has the appropriate key symbols" do
-      expect(journey_history.clear_current_journey).to include(:entry_station, :entry_zone,
+      expect(journey.clear_current_journey).to include(:entry_station, :entry_zone,
                                                         :exit_station, :exit_zone)
     end
 
     it 'checks the default journey history' do
-      expect(journey_history.clear_current_journey).to eq({entry_station: nil, entry_zone: nil,
+      expect(journey.clear_current_journey).to eq({entry_station: nil, entry_zone: nil,
                                                           exit_station: nil, exit_zone: nil})
     end
   end
 
   describe '#current_journey hash' do
     it 'checks if one journey is created on touch out' do
-      journey_history.start_journey(@station1)
-      journey_history.end_journey(@station2)
-      expect(journey_history.current_journey).to eq({entry_station: "Paddington", entry_zone: 1,
+      journey.start_journey(@station1)
+      journey.end_journey(@station2)
+      expect(journey.current_journey).to eq({entry_station: "Paddington", entry_zone: 1,
                                       exit_station: "Waterloo", exit_zone: 2})
     end
 
     it 'records entry station name and zone' do
-      journey_history.start_journey(@station1)
-      expect(journey_history.current_journey).to eq({entry_station: "Paddington", entry_zone: 1, exit_station: nil, exit_zone: nil})
+      journey.start_journey(@station1)
+      expect(journey.current_journey).to eq({entry_station: "Paddington", entry_zone: 1, exit_station: nil, exit_zone: nil})
     end
 
   end
